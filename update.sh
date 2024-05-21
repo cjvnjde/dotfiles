64353b0b9c1b3bcf8ba1073d9d7a3fbc1a4242f9
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2024-05-21 10:55:48 +0300

    update updater

--- update.sh
+++ update.sh
@@ -8,8 +8,12 @@ git submodule update --init --recursive
 
 echo "Updating and checking out submodules to main branch..."
 git submodule foreach --recursive '
-    git pull origin main
-    git checkout main
+    git fetch origin
+    branch=$(git symbolic-ref --short HEAD 2>/dev/null)
+    if [ "$branch" != "main" ]; then
+        git checkout main
+    fi
+    git reset --hard origin/main
 '
 
 echo "All submodules updated and checked out to main branch."
