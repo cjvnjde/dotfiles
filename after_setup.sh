b52e44653ca37efe7a0fe3b131c903d818b87bde
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2026-04-06 21:33:36 +0300

    restored the theme

---
+++ after_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! command -v bat &> /dev/null; then
+  echo "Error: bat is not installed. Please install it to use this script."
+
+  exit 1
+fi
+
+bat cache --build
+
+echo "bat cache rebuilt successfully."
+
+exit 0
